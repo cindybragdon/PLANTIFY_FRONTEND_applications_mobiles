fcc4629b913e6e379a52a33c5e59365a575abfd6
MFournier88 <maxime.fournier@collegemv.qc.ca>
2024-10-20 22:22:23 -0400

    First commit

---
+++ app/_layout.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Drawer } from 'expo-router/drawer';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { ThemeProvider } from '../contexts/ThemeContext';
+import CustomDrawerHeader from '../components/CustomDrawerHeader';
+
+const RootLayout = () => {
+  return (
+    <ThemeProvider>
+        <Layout/>
+    </ThemeProvider>
+  )
+}
+const Layout = () => {
+
+  return (
+    <>
+        <GestureHandlerRootView className="flex-1" >
+          <Drawer 
+              screenOptions={{
+                swipeEnabled:false,
+                // headerShown:false,
+                header: ({navigation}) => <CustomDrawerHeader navigation={navigation} tabName={""} />
+              }
+            }>
+              <Drawer.Screen name="index" options={{headerShown:false}} />
+              <Drawer.Screen name="auth" options={{headerShown:false}} />
+          </Drawer>
+        </GestureHandlerRootView>
+    </>
+  )
+}
+
+export default RootLayout
+
---
+++ app/index.jsx
@@ -0,0 +1,28 @@
+import { Text, View } from 'react-native'
+import React, { useState, useEffect} from 'react'
+import { useTheme } from '../contexts/ThemeContext'
+import { TouchableOpacity } from 'react-native'
+import { Link, useRouter} from 'expo-router'
+import { colorsPalette } from '../assets/colorsPalette'
+import Redirect from '../lib/redirect'
+const index = () => {
+    const { theme } = useTheme()
+    // const theme="dark"
+    const router = useRouter()
+    const colors = colorsPalette[theme]
+    // return <Redirect href="./1/profile" />;
+
+
+    return (
+        <View className={`flex-1 justify-evenly items-center`} style={{backgroundColor:colors.background_c1}} >
+            <Text className={`text-6xl font-bold tracking-[2px] text-center uppercase`} style={{color:colors.primary}} >ChatMV</Text>
+            <TouchableOpacity className={`rounded p-6`} style={{backgroundColor:colors.primary}} onPress={() => { router.push("./auth/signin")}}>
+                <Text className={`text-4xl`} style={{color:colors.lightText}} >Sign-in</Text>
+            </TouchableOpacity>
+            <Text class="text-3xl font-bold underline" style={{color:colors.text}}>If you don't already have an account <Link style={{color:colors.link}} href="./auth/signup">Sign-up</Link></Text>
+        </View>
+    )
+}
+
+
+export default index
