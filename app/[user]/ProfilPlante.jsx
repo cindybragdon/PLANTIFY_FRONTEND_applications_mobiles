e874dc1686576bdb334f143d949b168c13b64a7e
root <root@equipe1.10.10.0.5>
2024-12-20 22:10:03 +0000

    help fetch

--- app/[user]/ProfilPlante.jsx
+++ app/[user]/ProfilPlante.jsx
@@ -1,15 +1,30 @@
-import { Text, View, Image, TouchableOpacity, StyleSheet } from 'react-native'
-import React, { useState, useEffect } from 'react'
+import { Text, View, Image, TouchableOpacity, StyleSheet, Button } from 'react-native'
+import React, { useState, useEffect , useRef } from 'react'
 import { useTheme } from '../../contexts/ThemeContext'
 import { useFocusEffect, useRouter } from 'expo-router'
 import { colorsPalette } from '../../assets/colorsPalette'
-import { getIdFromJwt } from '../../lib/axios'
+import { getIdFromJwt, waterPlant  } from '../../lib/axios'
+import { CountdownCircleTimer } from 'react-native-countdown-circle-timer';
 
+//Animation Olivier https://www.npmjs.com/package/react-native-countdown-circle-timer
 
-const profilPlante = () => {
+const profilPlante = ({ plant }) => {
     const { theme } = useTheme()
     const router = useRouter()
     const colors = colorsPalette[theme]
+    const refresh = useRef(false)
+    //const [isWatered, setIsWatered] = useState(false);
+    const [key, setKey] = useState(0);
+
+    const handleWatering = async () => {
+      try {
+          await waterPlant(plant.id, plant.watering_interval);
+          setIsWatered(true);
+          setKey((prevKey) => prevKey + 1); 
+      } catch (error) {
+          console.error('Error watering plant:', error);
+      }
+  };
 
     return (
       
@@ -17,9 +32,45 @@ const profilPlante = () => {
 
             <Text className={`text-5xl uppercase font-bold text-center tracking-[4px]`} style={{ color: colors.green }}> CLAUDETTE 🪴</Text>
             <View className="items-center">
-            <Image className="h-[200] w-[200] mb-[28] mt-[24] rounded-full" style={styles.image} source={require('../../assets/images/plants.png')} />
             </View>
             <View className="p-12">
+            <View className='items-center'>
+            <TouchableOpacity 
+            >
+                <Image 
+                  className="w-40 h-40  rounded-full" 
+                  source={{}} /> 
+                
+                <View className="w-40 h-40 rounded-full bg-gray-400 justify-center items-center ">
+                  <Text className=" text-white font-bold">
+                    No profile picture
+                  </Text>
+                </View>
+            </TouchableOpacity>
+            </View>
+            
+            {/* <View>
+            <CountdownCircleTimer
+                key={key}
+                isPlaying={isWatered}
+                duration={plant.watering_interval * 3600} 
+                colors={['#4CAF50', '#FF9800', '#F44336']}
+                colorsTime={[7, 5, 2, 0]}
+                onComplete={() => {
+                    setIsWatered(false);
+                    return { shouldRepeat: false };
+                }}
+            >
+                {({ remainingTime }) => (
+                    <Text>
+                        {Math.floor(remainingTime / 3600)}h {Math.floor((remainingTime % 3600) / 60)}m
+                    </Text>
+                )}
+            </CountdownCircleTimer>
+            </View> 
+            
+            <Button title="Arrosée!" onPress={handleWatering} />
+            */}
             <Text className={`text-lg font-serif mb-[16] text-left`} style={{ color: colors.green }}> Surnom : </Text>
             <Text className={`text-lg font-serif mb-[16] text-left`} style={{ color: colors.green }}> Type : </Text>
             <Text className={`text-lg font-serif mb-[16] text-left`} style={{ color: colors.green }}> Fréquence d'arrosage : </Text>
--- app/[user]/addPlant.jsx
+++ app/[user]/addPlant.jsx
@@ -1,4 +1,4 @@
-import { Text, View, Image, TouchableOpacity, StyleSheet } from 'react-native'
+import {Text, View, TextInput, Dimensions, KeyboardAvoidingView, ActivityIndicator, ScrollView, Platform, Image, StyleSheet, TouchableOpacity} from 'react-native'
 import React, { useState, useEffect } from 'react'
 import { useTheme } from '../../contexts/ThemeContext'
 import { useFocusEffect, useRouter } from 'expo-router'
@@ -6,13 +6,24 @@ import { colorsPalette } from '../../assets/colorsPalette'
 import { getIdFromJwt } from '../../lib/axios'
 
 
+const  WIDTH_BTN = Dimensions.get('window').width - 56
+
 const addPlant = () => {
     const { theme } = useTheme()
     const router = useRouter()
     const colors = colorsPalette[theme]
+    const [form, setForm] = useState({name:"",type:"",age:"",location:""})
+
+    
 
     return (
-      
+        <KeyboardAvoidingView 
+        keyboardVerticalOffset={0}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        className="flex-1 justify-evenly items-center"
+        style={[{backgroundColor:colors.background}]}
+      >
+        
         <View className={`flex-1  justify-center`} style={{ backgroundColor: colors.background }}>
 
             <Text className={`text-5xl uppercase font-bold text-center tracking-[4px]`} style={{ color: colors.green }}> Ajouter une plante </Text>
@@ -20,30 +31,60 @@ const addPlant = () => {
             <Image className="h-[200] w-[200] mb-[28] mt-[24] rounded-full" style={styles.image} source={require('../../assets/images/succulent.png')} />
             </View>
             <View className="p-12">
-            {/* <TouchableOpacity 
-              onPress={() => {refresh.current = true;router.push("../camera")} }
-              className="rounded-full " 
-              
-              style={isEditing ? {borderWidth:4, borderColor:colors.lightAlert} : {}}
-            >
-              
-                <Image 
-                  className="w-40 h-40  rounded-full" 
-                  source={{uri:profilePic}} /> 
-                :
-                <View className="w-40 h-40 rounded-full bg-gray-400 justify-center items-center mb-10">
-                  <Text className=" text-white font-bold">
-                    No profile picture
-                  </Text>
+            <View className="" >
+                <TextInput
+                    className="justify-center py-5 rounded-lg text-center mb-2"
+                    style={[
+                    { width: WIDTH_BTN, color: colors.textgreen, backgroundColor: colors.green },
+                    {   }
+                    ]}
+                    onChangeText={(item) => setForm({ ...form, name: item })}
+                    placeholder="Entrez le nom de votre plante"  
+                    placeholderTextColor={colors.textgreen}
+                    value={form.name}
+                    />
+                    <TextInput
+                    className="justify-center py-5 rounded-lg text-center mb-2"
+                    style={[
+                    { width: WIDTH_BTN, color: colors.textgreen, backgroundColor: colors.green },
+                    {   }
+                    ]}
+                    onChangeText={(item) => setForm({ ...form, name: item })}
+                    placeholder="Entrez le type de votre plante"  
+                    placeholderTextColor={colors.textgreen}
+                    value={form.type}
+                    />
+                    <TextInput
+                    className="justify-center py-5 rounded-lg text-center mb-2"
+                    style={[
+                    { width: WIDTH_BTN, color: colors.textgreen, backgroundColor: colors.green },
+                    {   }
+                    ]}
+                    onChangeText={(item) => setForm({ ...form, name: item })}
+                    placeholder="Entrez l'age de votre plante"  
+                    placeholderTextColor={colors.textgreen}
+                    value={form.age}
+                    />
+                    <TextInput
+                    className="justify-center py-5 rounded-lg text-center mb-2"
+                    style={[
+                    { width: WIDTH_BTN, color: colors.textgreen, backgroundColor: colors.green },
+                    {   }
+                    ]}
+                    onChangeText={(item) => setForm({ ...form, name: item })}
+                    placeholder="Entrez la location de votre plante"  
+                    placeholderTextColor={colors.textgreen}
+                    value={form.location}
+                    />
                 </View>
-            </TouchableOpacity> */}
-            </View>
+                </View>
             <View className="items-center">
-            <TouchableOpacity className={`rounded-[12] py-[12] px-[24] `} style={{ backgroundColor: colors.green }} onPress={() => { router.push("./auth/") }}>
+            <TouchableOpacity className={`rounded-[12] py-[12] px-[24] `} style={{ backgroundColor: colors.green }} onPress={() => { router.push("./[user]/Collection") }}>
                   <Text className={`text-3xl text-center`} style={{ color: colors.lightText }}>Ajouter</Text>
             </TouchableOpacity>
             </View>
         </View>
+        </KeyboardAvoidingView>
   
     )
 
